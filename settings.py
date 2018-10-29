eb34799533b5b65c1a4747b0aed1d2b06c7184ab
doncov.eugene <doncov.eugene@alphaopen.com>
2018-10-29 18:11:49 +0300

    debug

--- settings.py
+++ settings.py
@@ -6,3 +6,5 @@ import os
 DEBUG = False
 # загрузим переменные настроек из json файла, чтобы не хранить в git
 globals().update(json.loads(open(os.path.join(os.path.dirname(__file__), os.pardir, 'alice.json')).read()))
+
+DEBUG = locals().get('DEBUG', 'False').lower() == 'true'
