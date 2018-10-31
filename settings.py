fb869ad1457be82e5ca916f336dc1ea89f3f11b5
doncov.eugene <doncov.eugene@alphaopen.com>
2018-10-31 04:07:15 +0300

    settings example

--- settings.py
+++ settings.py
@@ -7,3 +7,22 @@ import os
 globals().update(json.loads(open(os.path.join(os.path.dirname(__file__), os.pardir, 'alice.json')).read()))
 
 DEBUG = locals().get('DEBUG', 'False').lower() == 'true'
+
+
+""" Пример файла конфигурации alice.json
+{
+    "HOST": "x",
+    "PORT": 1,
+    "SITE": "http://127.0.0.1:10000",  для POST запроса кнопки
+    "_SITE": "https://somename.ru",  
+    "DEBUG": "True",
+    "LOG_DIR": "E:\\1"
+    
+    для emp_mos_api
+    "EMP_MOS_RU_TOKEN": "x",
+    "EMP_MOS_RU_USER_AGENT": "x",
+    "EMP_MOS_RU_GUID": "x",
+    "EMP_MOS_RU_DEV_USER_AGENT": "Android",
+    "EMP_MOS_RU_APP_VER": "x",
+}
+"""
