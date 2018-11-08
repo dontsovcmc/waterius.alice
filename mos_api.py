3256d0ee349adaf4ff33426500e0153232ae66b2
doncov.eugene <doncov.eugene@alphaopen.com>
2018-11-08 17:28:00 +0300

    timeout 1.5s

--- mos_api.py
+++ mos_api.py
@@ -8,4 +8,5 @@ api = MosAPI(token=settings.EMP_MOS_RU_TOKEN,
              guid=settings.EMP_MOS_RU_GUID,
              dev_user_agent=settings.EMP_MOS_RU_DEV_USER_AGENT,
              dev_app_version=settings.EMP_MOS_RU_APP_VER,
+             timeout=1.5,
              https_verify=True)
